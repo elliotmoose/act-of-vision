f46e4f959414b0897b1ecd17f83edea2ecc7f10c
Elliot Koh <appdev.moose@gmail.com>
2020-11-06 15:24:57 +0800

    add remainding icons

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -4,6 +4,10 @@ const Images = {
     tree: require('../../assets/images/tree.png'),
     spectacles: require('../../assets/images/spectacles.png'),
     x: require('../../assets/images/x.png'),
+    sutd: require('../../assets/images/sutd.png'),
+    concepts: require('../../assets/images/concepts.png'),
+    play: require('../../assets/images/play.png'),
+    
 }
 
 export default Images;
